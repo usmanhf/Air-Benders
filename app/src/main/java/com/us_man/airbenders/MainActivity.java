5aa27e55c984f9da0d89be11763dc2a0fb97a697
HumzaJaffri <heroichumza@gmail.com>
2018-01-28 08:53:43 -0600

    fixing radiobuttons

--- app/src/main/java/com/us_man/airbenders/MainActivity.java
+++ app/src/main/java/com/us_man/airbenders/MainActivity.java
@@ -71,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     @Override
+    public void onBackPressed() {}
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
