cd326bad5b5611cee7c210bfedf3347b85d905b1
Usman Farooqi <uhfarooqi@gmail.com>
2018-01-28 06:16:57 -0600

    created activities

---
+++ app/src/main/java/com/us_man/airbenders/Flights.java
@@ -0,0 +1,13 @@
+package com.us_man.airbenders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flights);
+    }
+}
