370f2390dcc99f9f12c9b5ec0d90a650a0713bbf
HumzaJaffri <heroichumza@gmail.com>
2018-01-28 05:21:50 -0600

    adds new parameters

--- app/src/main/java/com/us_man/airbenders/model/Passenger.java
+++ app/src/main/java/com/us_man/airbenders/model/Passenger.java
@@ -6,16 +6,26 @@ package com.us_man.airbenders.model;
 
 public class Passenger {
 
+    private final String[] SEAT_CHOICES = {"window", "aisle"};
+    private final String[] FLIGHT_CLASSES = {"F","B","E"};
+    private final String[] TALKING_PREFERENCES = {"yes", "no"};
+
     private String firstName;
     private String lastName;
     private String gender;
     private String email;
+    private String seatChoice;
+    private String flightClass;
+    private String talk;
 
     public Passenger(String firstName, String lastName, String gender, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
         this.email = email;
+        this.seatChoice = SEAT_CHOICES[(int)(Math.random()*2)];
+        this.flightClass = FLIGHT_CLASSES[(int)(Math.random()*3)];
+        this.talk = TALKING_PREFERENCES[(int)(Math.random()*2)];
     }
 
     public Passenger() {
@@ -38,12 +48,27 @@ public class Passenger {
         return email;
     }
 
+    public String getSeatChoice() {
+        return seatChoice;
+    }
+
+    public String getFlightClass() {
+        return flightClass;
+    }
+
+    public String getTalk() {
+        return talk;
+    }
+
     public String toString() {
         return "Passenger{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", gender='" + gender + '\'' +
                 ", email='" + email + '\'' +
+                ", seatChoice='" + seatChoice + '\'' +
+                ", flightClass='" + flightClass + '\'' +
+                ", getTalk='" + talk + '\'' +
                 "}";
     }
 
